84b992d3734300dfbe0732506a4a54cdae3010eb
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-14 13:54:17 -0300

    feat: Add User entity definition

---
+++ src/modules/users/infra/typeorm/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export default class User {
+  @PrimaryColumn({ type: 'varchar', length: 50 })
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp with time zone' })
+  updatedAt: Date;
+}
