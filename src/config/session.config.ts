ac3aa2da1c7797b58255fda96f2bbe829e23988a
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-15 01:37:56 -0300

    feat: Add date-fns and express-session dependencies, and configure session

---
+++ src/config/session.config.ts
@@ -0,0 +1,20 @@
+import session from 'express-session';
+
+import { addDays } from 'date-fns';
+
+const sessionConfig = session({
+  secret: process.env.SESSION_SECRET!,
+  name: 'token',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    path: '/api',
+    expires: addDays(new Date(), 1),
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: true,
+  },
+});
+
+export default sessionConfig;
