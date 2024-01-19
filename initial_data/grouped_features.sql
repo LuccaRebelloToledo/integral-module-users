45a5f97a5b16d68b336808de2de189c0ef43ea18
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-19 11:09:08 -0300

    chore: Add user full access to grouped features

--- initial_data/grouped_features.sql
+++ initial_data/grouped_features.sql
@@ -1,1 +1,2 @@
-INSERT INTO public.grouped_features ("featureGroupId", "featureId") VALUES('FWwq9ec55ZQUYpIKsCBUk', 'mUKeu2oGkLzYlQDqlCFRd');
+-- User Full Access
+INSERT INTO public.grouped_features ("featureGroupId", "featureId") VALUES('FWwq9ec55ZQUYpIKsCBUk', 'iAig39hONXsJzI1EuW9ZY');
