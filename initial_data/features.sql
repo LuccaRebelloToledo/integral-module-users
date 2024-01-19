8a6a148dcf2707c7cbc6350ad471c3a1cb41af65
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-19 10:48:21 -0300

    feat: Add initial data for features and users

--- initial_data/features.sql
+++ initial_data/features.sql
@@ -1,1 +1,14 @@
-INSERT INTO public.features (id, "key", "name") values ('mUKeu2oGkLzYlQDqlCFRd', 'test', 'test')
+-- Features
+INSERT INTO public.features (id, "key", "name") values ('XA5tYtZFuFE079lOkkJDZ', 'full:features', 'Full access to features');
+INSERT INTO public.features (id, "key", "name") values ('RL9MGPPnEi70x7Ev8eudC', 'show:features', 'Show features details');
+INSERT INTO public.features (id, "key", "name") values ('nfp8xjjO29xp3FRyyHJ7i', 'list:features', 'List all features');
+
+-- Feature Groups
+
+-- Users
+INSERT INTO public.features (id, "key", "name") values ('iAig39hONXsJzI1EuW9ZY', 'full:users', 'Full access to users');
+INSERT INTO public.features (id, "key", "name") values ('EvNFvcTp0GFocXfqB6Mq1', 'show:users', 'Show user details');
+INSERT INTO public.features (id, "key", "name") values ('DFtIzMaDYs5QJ3oB7w69P', 'list:users', 'List all users');
+INSERT INTO public.features (id, "key", "name") values ('DbNvuUQOgYgC5UQLe0Ldj', 'create:users', 'Create new users');
+INSERT INTO public.features (id, "key", "name") values ('uwIPn9g5kPYKtEb569XY4', 'update:users', 'Update user details');
+INSERT INTO public.features (id, "key", "name") values ('So24YhaZkb63omL0W5SJA', 'delete:users', 'Delete users');
