b53fb02efa33116b54f8c8b7524f9006ea4a65b7
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-19 11:43:52 -0300

    chore: Add new feature group 'Member'

--- initial_data/feature_groups.sql
+++ initial_data/feature_groups.sql
@@ -1,1 +1,2 @@
 INSERT INTO public.feature_groups (id, "key", "name") VALUES ('FWwq9ec55ZQUYpIKsCBUk', 'MASTER', 'Master');
+INSERT INTO public.feature_groups (id, "key", "name") VALUES ('6P8s76YxCtSMgmKDx49dV', 'MEMBER', 'Member');
