97f77e54b8e919ec54ac47f54dbdd356af480a29
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-20 13:53:53 -0300

    chore: TO DO pagination to findAll method

--- src/modules/users/infra/typeorm/repositories/user.repository.ts
+++ src/modules/users/infra/typeorm/repositories/user.repository.ts
@@ -15,6 +15,8 @@ export default class UserRepository implements UserRepositoryInterface {
   }
 
   public async findAll(): Promise<User[]> {
+    // TO DO Pagination
+
     return await this.userRepository.find({
       relations: ['featureGroup', 'standaloneFeatures'],
     });
