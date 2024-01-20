c00d834558ae0b0d38e5c913ee47f6d5a6f79472
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-20 14:25:36 -0300

    feat: Add feature controller

---
+++ src/modules/features/infra/http/controllers/feature.controller.ts
@@ -0,0 +1,85 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { container } from 'tsyringe';
+
+import ListFeaturesService from '@modules/features/services/list-features.service';
+import ListFeaturesByUserIdService from '@modules/features/services/list-features-by-user-id.service';
+import ListFeaturesByFeatureGroupIdService from '@modules/features/services/list-features-by-feature-group-id.service';
+import ListFeaturesByKeyOrNameService from '@modules/features/services/list-features-by-key-or-name.service';
+import ShowFeaturesService from '@modules/features/services/show-features.service';
+
+export default class FeatureController {
+  public async list(
+    _request: Request,
+    response: Response,
+    _next: NextFunction,
+  ): Promise<Response> {
+    const listFeaturesService = container.resolve(ListFeaturesService);
+    const features = await listFeaturesService.execute();
+
+    return response.json(features);
+  }
+
+  public async listByUserId(
+    request: Request,
+    response: Response,
+    _next: NextFunction,
+  ): Promise<Response> {
+    const { id: userId } = request.params;
+
+    const listFeaturesByUserIdService = container.resolve(
+      ListFeaturesByUserIdService,
+    );
+    const features = await listFeaturesByUserIdService.execute(userId);
+
+    return response.json(features);
+  }
+
+  public async listByFeatureGroupId(
+    request: Request,
+    response: Response,
+    _next: NextFunction,
+  ): Promise<Response> {
+    const { id: featureGroupId } = request.params;
+
+    const listFeaturesByFeatureGroupIdService = container.resolve(
+      ListFeaturesByFeatureGroupIdService,
+    );
+    const features = await listFeaturesByFeatureGroupIdService.execute(
+      featureGroupId,
+    );
+
+    return response.json(features);
+  }
+
+  public async listByKeyOrName(
+    request: Request,
+    response: Response,
+    _next: NextFunction,
+  ): Promise<Response> {
+    const { key, name } = request.body;
+
+    const listFeaturesByKeyOrNameService = container.resolve(
+      ListFeaturesByKeyOrNameService,
+    );
+    const features = await listFeaturesByKeyOrNameService.execute({
+      key,
+      name,
+    });
+
+    return response.json(features);
+  }
+
+  public async show(
+    request: Request,
+    response: Response,
+    _next: NextFunction,
+  ): Promise<Response> {
+    const { id: featureId } = request.params;
+
+    const showFeaturesService = container.resolve(ShowFeaturesService);
+    const feature = await showFeaturesService.execute(featureId);
+
+    return response.json(feature);
+  }
+}
