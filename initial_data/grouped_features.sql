9517aa05ed698f4b4cf080fc78af25edeeda9112
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-20 14:27:06 -0300

    feat: Add new features to grouped_features table

--- initial_data/grouped_features.sql
+++ initial_data/grouped_features.sql
@@ -1,2 +1,6 @@
--- User Full Access
+-- MASTER
+
+-- Features Full Access
 INSERT INTO public.grouped_features ("featureGroupId", "featureId") VALUES('FWwq9ec55ZQUYpIKsCBUk', 'iAig39hONXsJzI1EuW9ZY');
+-- User Full Access
+INSERT INTO public.grouped_features ("featureGroupId", "featureId") VALUES('FWwq9ec55ZQUYpIKsCBUk', 'XA5tYtZFuFE079lOkkJDZ');
