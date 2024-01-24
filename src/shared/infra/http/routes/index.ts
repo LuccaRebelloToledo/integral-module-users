dc6b4440e8044509db1612e1ad7e8f526877f859
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-24 10:38:04 -0300

    feat: Add feature groups routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import featuresRoutes from '@modules/features/infra/http/routes/feature.routes';
+import featureGroupsRoutes from '@modules/features/infra/http/routes/feature-group.routes';
 import userRoutes from '@modules/users/infra/http/routes/user.routes';
 import sessionRoutes from '@modules/users/infra/http/routes/session.routes';
 
 const routes = Router();
 
 routes.use('/features', featuresRoutes);
+routes.use('feature-groups', featureGroupsRoutes);
 routes.use('/users', userRoutes);
 routes.use('/sessions', sessionRoutes);
 
