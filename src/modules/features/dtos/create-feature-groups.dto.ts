c5777770e25d38e4e51f4806e6a51c4a2ed7e60e
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-24 11:00:46 -0300

    fix: Add id property to CreateFeatureGroupsDTO

--- src/modules/features/dtos/create-feature-groups.dto.ts
+++ src/modules/features/dtos/create-feature-groups.dto.ts
@@ -1,6 +1,7 @@
 import Feature from '../infra/typeorm/entities/feature.entity';
 
 export default class CreateFeatureGroupsDTO {
+  id: string;
   key: string;
   name: string;
   features: Feature[];
