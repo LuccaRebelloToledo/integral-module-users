b9a59a7817e2a656f2eaf3f58ec1de16241f68fc
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-01-24 10:37:10 -0300

    feat: Add new feature groups and update feature details

--- initial_data/features.sql
+++ initial_data/features.sql
@@ -1,17 +1,25 @@
 -- Features
 INSERT INTO public.features (id, "key", "name") values ('XA5tYtZFuFE079lOkkJDZ', 'full:features', 'Full access to features');
-INSERT INTO public.features (id, "key", "name") values ('RL9MGPPnEi70x7Ev8eudC', 'show:features', 'Show features details');
 INSERT INTO public.features (id, "key", "name") values ('nfp8xjjO29xp3FRyyHJ7i', 'list:features', 'List all features');
 INSERT INTO public.features (id, "key", "name") values ('KJhcZzzSEQAuP22UZ45ti', 'list-users:features', 'List all features by user id');
 INSERT INTO public.features (id, "key", "name") values ('BlZWK9WPf6DVTvMmqRaCa', 'list-feature-groups:features', 'List all features by feature group id');
 INSERT INTO public.features (id, "key", "name") values ('AjNyi0NOeKPY9tnU2t2dg', 'list-key-or-name:features', 'List all features by key or name');
+INSERT INTO public.features (id, "key", "name") values ('RL9MGPPnEi70x7Ev8eudC', 'show:features', 'Show features details');
 
 -- Feature Groups
 
+INSERT INTO public.features (id, "key", "name") values ('Vt399peAJDrq36bgU6I2l', 'full:feature-groups', 'Full access to feature groups');
+INSERT INTO public.features (id, "key", "name") values ('kBV3bAql5w8VC9n7h2Wcj', 'list:feature-groups', 'List all feature groups');
+INSERT INTO public.features (id, "key", "name") values ('pXXyAq7KQrqGrxKMeNBbE', 'list-key-or-name:feature-groups', 'List all feature groups by key or name');
+INSERT INTO public.features (id, "key", "name") values ('b8TU9sLcSwwUp9IXN9CYq', 'show:feature-groups', 'Show feature groups details');
+INSERT INTO public.features (id, "key", "name") values ('BMHz3DvG7mDmRqH14dhDF', 'create:feature-groups', 'Create new feature groups');
+INSERT INTO public.features (id, "key", "name") values ('faKmSTMuCXg6McCCufVJU', 'update:feature-groups', 'Update feature groups details');
+INSERT INTO public.features (id, "key", "name") values ('POqC31p2BCWqDdB3UNphW', 'delete:feature-groups', 'Delete feature groups');
+
 -- Users
 INSERT INTO public.features (id, "key", "name") values ('iAig39hONXsJzI1EuW9ZY', 'full:users', 'Full access to users');
-INSERT INTO public.features (id, "key", "name") values ('EvNFvcTp0GFocXfqB6Mq1', 'show:users', 'Show users details');
 INSERT INTO public.features (id, "key", "name") values ('DFtIzMaDYs5QJ3oB7w69P', 'list:users', 'List all users');
+INSERT INTO public.features (id, "key", "name") values ('EvNFvcTp0GFocXfqB6Mq1', 'show:users', 'Show users details');
 INSERT INTO public.features (id, "key", "name") values ('DbNvuUQOgYgC5UQLe0Ldj', 'create:users', 'Create new users');
 INSERT INTO public.features (id, "key", "name") values ('uwIPn9g5kPYKtEb569XY4', 'update:users', 'Update users details');
 INSERT INTO public.features (id, "key", "name") values ('So24YhaZkb63omL0W5SJA', 'delete:users', 'Delete users');
--- initial_data/grouped_features.sql
+++ initial_data/grouped_features.sql
@@ -2,5 +2,7 @@
 
 -- Features Full Access
 INSERT INTO public.grouped_features ("featureGroupId", "featureId") VALUES('FWwq9ec55ZQUYpIKsCBUk', 'iAig39hONXsJzI1EuW9ZY');
+-- Feature Groups Full Access
+INSERT INTO public.grouped_features ("featureGroupId", "featureId") VALUES('FWwq9ec55ZQUYpIKsCBUk', 'Vt399peAJDrq36bgU6I2l');
 -- User Full Access
 INSERT INTO public.grouped_features ("featureGroupId", "featureId") VALUES('FWwq9ec55ZQUYpIKsCBUk', 'XA5tYtZFuFE079lOkkJDZ');
