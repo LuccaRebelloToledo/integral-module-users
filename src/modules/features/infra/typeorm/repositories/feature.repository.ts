bbbf83fa5124d4e5eba75c63d11abdc15e90bf97
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-02-05 14:39:20 -0300

    feat: add ListFeaturesRepositoryParamsDTO and update findAll method

--- src/modules/features/infra/typeorm/repositories/feature.repository.ts
+++ src/modules/features/infra/typeorm/repositories/feature.repository.ts
@@ -9,6 +9,8 @@ import { ILike, Repository } from 'typeorm';
 import FeatureRepositoryInterface from '@modules/features/repositories/feature.repository.interface';
 import Feature from '../entities/feature.entity';
 
+import ListFeaturesRepositoryParamsDTO from '@modules/features/dtos/list-features-repository-params.dto';
+import ListRepositoryResponseDTO from '@shared/dtos/list-repository-response.dto';
 import FindFeaturesByKeyOrNameDTO from '@modules/features/dtos/find-features-by-key-or-name.dto';
 import CreateFeaturesDTO from '@modules/features/dtos/create-features.dto';
 
@@ -21,8 +23,36 @@ export default class FeatureRepository implements FeatureRepositoryInterface {
       : AppDataSource.getRepository(Feature);
   }
 
-  public async findAll(): Promise<Feature[]> {
-    return await this.featureRepository.find();
+  public async findAll({
+    take,
+    skip,
+    sort,
+    order,
+    key,
+    name,
+  }: ListFeaturesRepositoryParamsDTO): Promise<
+    ListRepositoryResponseDTO<Feature>
+  > {
+    const query = this.featureRepository
+      .createQueryBuilder('features')
+      .take(take)
+      .skip(skip)
+      .orderBy(`features.${sort}`, order as 'ASC' | 'DESC');
+
+    if (key) {
+      query.andWhere({ key: ILike(`%${key}%`) });
+    }
+
+    if (name) {
+      query.andWhere({ name: ILike(`%${name}%`) });
+    }
+
+    const [items, total] = await query.getManyAndCount();
+
+    return {
+      items,
+      total,
+    };
   }
 
   public async findAllByUserId(userId: string): Promise<Feature[]> {
--- src/modules/features/repositories/feature.repository.interface.ts
+++ src/modules/features/repositories/feature.repository.interface.ts
@@ -1,10 +1,14 @@
 import Feature from '../infra/typeorm/entities/feature.entity';
 
+import ListFeaturesRepositoryParamsDTO from '../dtos/list-features-repository-params.dto';
+import ListRepositoryResponseDTO from '@shared/dtos/list-repository-response.dto';
 import FindFeaturesByKeyOrNameDTO from '../dtos/find-features-by-key-or-name.dto';
 import CreateFeaturesDTO from '../dtos/create-features.dto';
 
 export default interface FeatureRepositoryInterface {
-  findAll(): Promise<Feature[]>;
+  findAll(
+    params: ListFeaturesRepositoryParamsDTO,
+  ): Promise<ListRepositoryResponseDTO<Feature>>;
   findAllByUserId(userId: string): Promise<Feature[]>;
   findAllFeaturesByFeatureGroupId(featureGroupId: string): Promise<Feature[]>;
   findById(featureId: string): Promise<Feature | null>;
