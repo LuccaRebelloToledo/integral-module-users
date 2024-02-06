30259cc0b924a239dc1a71fc064ad95e8c9fee66
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-02-06 09:42:42 -0300

    refactor: list-users.service.spec.ts

--- src/modules/users/services/list-users.service.spec.ts
+++ src/modules/users/services/list-users.service.spec.ts
@@ -13,6 +13,31 @@ let userRepository: UserRepository;
 let featureGroupRepository: FeatureGroupRepository;
 let listUsersService: ListUsersService;
 
+const payload = {
+  page: '1',
+  limit: '5',
+  sort: 'createdAt',
+  order: 'DESC',
+  name: undefined,
+  email: undefined,
+};
+
+const { pageParsed, limitParsed } = convertPageAndLimitToInt(
+  payload.page,
+  payload.limit,
+);
+
+const skip = calculateSkip(pageParsed, limitParsed);
+
+const payloadParsed = {
+  take: limitParsed,
+  skip: skip,
+  sort: 'createdAt',
+  order: 'DESC',
+  name: undefined,
+  email: undefined,
+};
+
 describe('ListUsersService', () => {
   beforeAll(async () => {
     await TestAppDataSource.initialize();
@@ -40,31 +65,6 @@ describe('ListUsersService', () => {
   });
 
   test('should throw an error if no user is found', async () => {
-    const payload = {
-      page: '1',
-      limit: '5',
-      sort: 'createdAt',
-      order: 'DESC',
-      name: undefined,
-      email: undefined,
-    };
-
-    const { pageParsed, limitParsed } = convertPageAndLimitToInt(
-      payload.page,
-      payload.limit,
-    );
-
-    const skip = calculateSkip(pageParsed, limitParsed);
-
-    const payloadParsed = {
-      take: limitParsed,
-      skip: skip,
-      sort: 'createdAt',
-      order: 'DESC',
-      name: undefined,
-      email: undefined,
-    };
-
     jest.spyOn(userRepository, 'findAll');
 
     await expect(listUsersService.execute(payload)).rejects.toThrow(
@@ -76,8 +76,8 @@ describe('ListUsersService', () => {
     expect(userRepository.findAll).toHaveBeenCalledTimes(1);
   });
 
-  it('should return a list of users', async () => {
-    const createdUserOne = await userRepository.create({
+  it('should be return a list of users', async () => {
+    const userOne = await userRepository.create({
       id: '1',
       name: 'John Doe',
       email: 'johndoe@example.com',
@@ -85,38 +85,13 @@ describe('ListUsersService', () => {
       featureGroupId: '1',
     });
 
-    const createdUserTwo = await userRepository.create({
+    const userTwo = await userRepository.create({
       id: '2',
       name: 'John Doe',
       email: 'johndoe2@example.com',
       password: '123456',
       featureGroupId: '1',
     });
-
-    const payload = {
-      page: '1',
-      limit: '5',
-      sort: 'createdAt',
-      order: 'DESC',
-      name: undefined,
-      email: undefined,
-    };
-
-    const { pageParsed, limitParsed } = convertPageAndLimitToInt(
-      payload.page,
-      payload.limit,
-    );
-
-    const skip = calculateSkip(pageParsed, limitParsed);
-
-    const payloadParsed = {
-      take: limitParsed,
-      skip: skip,
-      sort: 'createdAt',
-      order: 'DESC',
-      name: undefined,
-      email: undefined,
-    };
 
     jest.spyOn(userRepository, 'findAll');
 
@@ -130,7 +105,7 @@ describe('ListUsersService', () => {
     expect(pagination.current).toEqual(pageParsed);
     expect(items).toHaveLength(2);
     expect(totalItems).toEqual(2);
-    expect(items[0].id).toEqual(createdUserOne.id);
-    expect(items[1].id).toEqual(createdUserTwo.id);
+    expect(items[0].id).toEqual(userOne.id);
+    expect(items[1].id).toEqual(userTwo.id);
   });
 });
