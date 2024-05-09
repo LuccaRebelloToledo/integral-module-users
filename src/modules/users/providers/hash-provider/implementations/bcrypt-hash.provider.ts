e4fbdf198a51e446e1e9adf80ce07913b93dda15
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-05-09 11:51:50 -0300

    feat: update BCryptHashProvider to use async/await for hash generation

--- src/modules/users/providers/hash-provider/implementations/bcrypt-hash.provider.ts
+++ src/modules/users/providers/hash-provider/implementations/bcrypt-hash.provider.ts
@@ -2,14 +2,12 @@ import { compare, hash } from 'bcryptjs';
 
 import HashProviderInterface from '../models/hash.provider.interface';
 
-class BCryptHashProvider implements HashProviderInterface {
+export default class BCryptHashProvider implements HashProviderInterface {
   public async generateHash(payload: string): Promise<string> {
-    return hash(payload, 8);
+    return await hash(payload, 8);
   }
 
   public async compareHash(payload: string, hashed: string): Promise<boolean> {
     return await compare(payload, hashed);
   }
 }
-
-export default BCryptHashProvider;
