02950dd4c6f6baffa1deda59858716884f4d056d
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-05-09 13:07:44 -0300

    refactor: update get-active-data-source to use environments

--- src/shared/utils/get-active-data-source.utils.ts
+++ src/shared/utils/get-active-data-source.utils.ts
@@ -1,7 +1,9 @@
-import { isTesting } from '@shared/infra/environments/env';
+import { env } from '@shared/infra/environments/environments';
 
 import { AppDataSource } from '@shared/infra/typeorm/data-sources/data-source';
 import { TestAppDataSource } from '@shared/infra/typeorm/data-sources/test-data-source';
+
+const isTesting = env.NODE_ENV === 'test';
 
 export const getActiveDataSource = () => {
   return isTesting ? TestAppDataSource : AppDataSource;
