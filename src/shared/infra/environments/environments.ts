a3d5f612e4873808d836ce2a16c90e80a7edb91f
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-05-09 13:07:27 -0300

    refactor: rename env.ts to environments.ts and update schema validation

--- src/shared/infra/environments/environments.ts
+++ src/shared/infra/environments/environments.ts
@@ -1,34 +1,28 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 
-export const isTesting = process.env.NODE_ENV === 'test';
-
-const dotenvConfigOutput = dotenv.config({
-  path: isTesting ? '.env.test' : '.env',
-});
+import process from 'node:process';
 
 import { Joi } from 'celebrate';
 
-const envSchema = Joi.object({
+const environmentsSchema = Joi.object({
   NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRATION: Joi.string().required(),
   DSN: Joi.string().uri().required(),
-  PORT: Joi.string().required(),
+  PORT: Joi.number().integer().positive().required(),
   PG_HOST: Joi.string().required(),
-  PG_PORT: Joi.string().required(),
+  PG_PORT: Joi.number().integer().positive().required(),
   PG_USER: Joi.string().required(),
   PG_PASS: Joi.string().required(),
   PG_DB: Joi.string().required(),
   REDIS_HOST: Joi.string().required(),
-  REDIS_PORT: Joi.string().required(),
-});
+  REDIS_PORT: Joi.number().integer().positive().required(),
+  REDIS_PASS: Joi.string().required(),
+}).required();
 
-const { error, value: envVars } = envSchema.validate(
-  dotenvConfigOutput.parsed,
-  {
-    allowUnknown: true,
-  },
-);
+const { error, value: envVars } = environmentsSchema.validate(process.env, {
+  allowUnknown: true,
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
